84bdcc9cec920d51a597e483742a6729eec33880
eckso <eckso@eckso.io>
2021-10-25 06:00:00 -0400

    revert show constraint id in rapl table

--- src/format/intel_rapl.rs
+++ src/format/intel_rapl.rs
@@ -10,7 +10,6 @@ pub fn format() -> Option<String> {
     for pol in pols {
         let id = if let Some(id) = pol.id { id } else { continue; };
         tab.row(&[
-            pol.name.clone().unwrap_or_else(dot),
             id.zone.to_string(),
             id.subzone.map(|v| v.to_string()).unwrap_or_else(dot),
             pol.enabled.map(|v| v.to_string()).unwrap_or_else(dot),
@@ -19,26 +18,16 @@ pub fn format() -> Option<String> {
                 .and_then(|v| v
                     .iter()
                     .find(|p| matches!(p.name.as_deref(), Some("long_term")))
-                    .map(|v| {
-                        let id = v.id.map(|v| v.to_string()).unwrap_or_else(dot);
-                        let v = v.power_limit_uw
-                            .map(|v| format!("{:.1}", Power::from_microwatts(v as f64)))
-                            .unwrap_or_else(dot);
-                        format!("{}: {}", id, v)
-                    }))
+                    .and_then(|v| v.power_limit_uw)
+                    .map(|v| format!("{:.1}", Power::from_microwatts(v as f64))))
                 .unwrap_or_else(dot),
             pol.constraints
                 .as_deref()
                 .and_then(|v| v
                     .iter()
                     .find(|p| matches!(p.name.as_deref(), Some("short_term")))
-                    .map(|v| {
-                        let id = v.id.map(|v| v.to_string()).unwrap_or_else(dot);
-                        let v = v.power_limit_uw
-                            .map(|v| format!("{:.1}", Power::from_microwatts(v as f64)))
-                            .unwrap_or_else(dot);
-                        format!("{}: {}", id, v)
-                    }))
+                    .and_then(|v| v.power_limit_uw)
+                    .map(|v| format!("{:.1}", Power::from_microwatts(v as f64))))
                 .unwrap_or_else(dot),
             pol.energy_uj
                 .map(|v| format!("{:.1}", Energy::from_joules((v as f64/10f64.powf(6.)) as f64)))
